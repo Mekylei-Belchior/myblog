fc28cb874a93fe61314535e2e50b5fe6d13ebbe2
Mekylei Belchior <mekylei@msn.com>
2025-03-18 21:44:14 -0300

    simplificando DTOs para Records

--- api/src/main/java/br/com/mekylei/myblog/dtos/news/NewsDTO.java
+++ api/src/main/java/br/com/mekylei/myblog/dtos/news/NewsDTO.java
@@ -4,46 +4,5 @@ import jakarta.validation.constraints.NotBlank;
 
 import java.util.List;
 
-public class NewsDTO {
-
-    @NotBlank
-    private String title;
-    @NotBlank
-    private String author;
-    @NotBlank
-    private String content;
-
-    private List<String> tags;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
-    }
+public record NewsDTO(@NotBlank String title, @NotBlank String author, @NotBlank String content, List<String> tags) {
 }
