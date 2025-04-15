3ad1fd25155428acf42612fa23ab66f6d1550e12
Mekylei Belchior <mekylei@msn.com>
2025-04-15 13:30:21 -0300

    ajustado a responsividade e o layout da home

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FooterService } from '../shared/services/footer-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,19 @@ import { FooterService } from '../shared/services/footer-service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private footerService: FooterService) {}
+  constructor(
+    private footerService: FooterService,
+  private router: Router) {}
 
   ngOnInit(): void {
     this.footerService.show();
   }
+
+  /**
+   * Navigate to the POSTAGEM page
+   */
+  public goPostagem(): void {
+    this.router.navigate(['postagem']);
+  }
+  
 }
