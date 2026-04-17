8fa5c3924a063015701a8be87f274fc989e4d691
Mekylei Belchior <mekylei@msn.com>
2026-04-17 10:33:09 -0300

    Altera nome do jar no script de limpeza e construcao

--- api/clean-build-run.sh
+++ api/clean-build-run.sh
@@ -6,4 +6,4 @@ rm -rf build
 rm -rf out
 ./gradlew clean --refresh-dependencies
 JWT_SECRET=w5j4+3X7f8K9l0m1n2o3p4q5r6s7t8u9v0w1x2y3z4A5B6C7D8E9F0G1H ./gradlew build --stacktrace
-java -DJWT_SECRET=w5j4+3X7f8K9l0m1n2o3p4q5r6s7t8u9v0w1x2y3z4A5B6C7D8E9F0G1H -jar ./build/libs/api-0.0.1-SNAPSHOT.jar
+java -DJWT_SECRET=w5j4+3X7f8K9l0m1n2o3p4q5r6s7t8u9v0w1x2y3z4A5B6C7D8E9F0G1H -jar ./build/libs/api-myblog.jar
